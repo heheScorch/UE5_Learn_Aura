9c7e4022b9a378b1bdd5533b65ddadf7446c63d2
heheScorch <13588014729@163.com>
2024-06-16 19:16:47 +0800

    add enemy mouse hover outline

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -14,7 +14,10 @@ UCLASS()
 class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-
+	
+public:
+	AAuraEnemy();
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+
 };
--- Source/Aura/Public/Interaction/EnemyInterface.h
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -20,6 +20,7 @@ class AURA_API IEnemyInterface
 {
 	GENERATED_BODY()
 
+public:
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 	virtual void HighlightActor() = 0;
 	virtual void UnHighlightActor() = 0;
